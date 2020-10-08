73786b6855b9c7f5c6b1f2e1ad56f049d714cce5
Oli Pfeffer <pfeffero@amazon.com>
2020-10-07 21:41:59 -0400

    Initial Commit

---
+++ Xcode12Sandbox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xcode12Sandbox
+//
+//  Created by Pfeffer, Oliver on 10/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
